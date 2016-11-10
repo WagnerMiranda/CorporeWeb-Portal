dcb145fc95de590c39e0e33b1539f823f4f40afd
WagnerMiranda <wamiranda@gmail.com>
2016-11-10 14:14:03 -0200

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CorporeWebPortal.Startup))]
+namespace CorporeWebPortal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
